5fda8e54d9227821cfb19e38fae81c83c335ea7d
rashedul25389 <33155802+rashedul25389@users.noreply.github.com>
2017-12-04 07:29:07 -0800

    class-21

---
+++ Class-21/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        $(".homepage-slides").owlCarousel({
+            items: 1,
+            nav: false,
+            dots: true,
+            loop: true,
+            autoplay: false,
+            autoplay: true,
+            
+        });
+        
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
