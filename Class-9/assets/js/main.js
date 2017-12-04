1d8424a48d34d044a29fdd684796e29ae8332a62
rashedul25389 <33155802+rashedul25389@users.noreply.github.com>
2017-12-04 07:09:46 -0800

    class-9

---
+++ Class-9/assets/js/main.js
@@ -0,0 +1,33 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+        
+        $(".client-testimonial-carousel").owlCarousel({
+            items: 3,
+            margin: 30,
+            nav: false,
+            dots: true,
+            loop: true,
+        });
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
