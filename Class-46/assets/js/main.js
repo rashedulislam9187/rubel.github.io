ab8d9d92882fe53962e6a995f8a08235e58a7fc5
rashedul25389 <33155802+rashedul25389@users.noreply.github.com>
2018-02-04 12:41:32 +0600

    class-44

---
+++ Class-46/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        
+        $(".features-trailors-carousel").owlCarousel({
+            items: 1,
+            autoplay: false,
+            loop: true,
+            nav: false,
+            dots: true,
+            
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
---
+++ Class-47/assets/js/main.js
@@ -0,0 +1,30 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        
+        $(".testimonial-carousel").owlCarousel({
+            items: 1,
+            autoplay: true,
+            loop: true,
+            nav: false,
+            dots: true,
+            
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
